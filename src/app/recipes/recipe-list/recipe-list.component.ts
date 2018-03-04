58259937a3e1e42d23c9902c7c6f78c9149c6bc8
Grzegorz <grzegorz.petrykowski@gmail.com>
2018-03-04 21:28:02 +0000

    Recipe loop setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://placeimg.com/640/480/people'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.cipd.co.uk/Images/cipd-scotland_tcm18-8662_w1200_n.png')
+  ];
 
   constructor() { }
 
